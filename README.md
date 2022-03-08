421be924f799c844c9a2da70c89618091a310355
crsnbogdan <crsnbogdan@gmail.com>
2022-03-09 00:09:08 +0200

    added form styling rules and made slight modifications to the script to ensure that it works as expected

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 TITLE: Form Validation and Constraint Validation API
 
-VERSION NUMBER:1.0.2
+VERSION NUMBER:1.0.3
 
 DESCRIPTION: Project created for the purpose of understanding the Constraint Validation API and Form Validation using JavaScript
 
 PROJECT STATUS: In Progress
 
 TO DO:
-- add CSS styling rules
 - display a message when the form is submitted
 
 TECHNOLOGIES USED: HTML, CSS, JS, NPM, Webpack, ESLint, NPM, Prettier, Constraint Validation API
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import '../src/style.css'
-const form = document.getElementsByTagName("form")[0];
+import "../src/style.css";
+const form = document.getElementById("form");
 
 const emailField = document.getElementById("email");
 const emailErrorField = document.getElementById("email-error");
@@ -12,161 +12,179 @@ const passwordField = document.getElementById("password");
 const passwordErrorField = document.getElementById("password-error");
 const passwordConfirmationField = document.getElementById("confirmation");
 const passwordConfirmationErrorField =
-    document.getElementById("confirmation-error");
+  document.getElementById("confirmation-error");
 
-function showError(errorField, errorMessage) {
-    errorField.textContent = errorMessage;
-    errorField.classList.add("error");
-    if (errorField.classList.contains("valid")) {
-        errorField.classList.remove("valid");
-    }
+function showError(errorField, errorMessage, InputElement) {
+  errorField.textContent = errorMessage;
+  InputElement.classList.add("error");
+  if (InputElement.classList.contains("valid")) {
+    InputElement.classList.remove("valid");
+  }
 }
 
-function removeError(errorField) {
-    errorField.textContent = "";
-    errorField.classList.remove("error");
-    errorField.classList.add("valid");
+function removeError(errorField, inputElement) {
+  errorField.textContent = "";
+  inputElement.classList.remove("error");
+  inputElement.classList.add("valid");
 }
 
-emailField.addEventListener("input", function(event) {
-    if (emailField.validity.valueMissing) {
-        showError(emailErrorField, "You need to input an email address.");
-    }
-    if (!emailField.validity.tooShort) {
-        removeError(emailErrorField);
-    } else if (emailField.validity.tooShort) {
-        showError(emailErrorField, "The email address is too short.");
-    }
-    if (emailField.validity.typeMismatch) {
-        showError(emailErrorField, "This is not a valid email address.");
-    }
-    if (emailField.validity.valid) {
-        removeError(emailErrorField);
-    }
+emailField.addEventListener("input", function (event) {
+  if (emailField.validity.valueMissing) {
+    showError(
+      emailErrorField,
+      "You need to input an email address.",
+      emailField
+    );
+  }
+  if (!emailField.validity.tooShort) {
+    removeError(emailErrorField, emailField);
+  } else if (emailField.validity.tooShort) {
+    showError(emailErrorField, "The email address is too short.", emailField);
+  }
+  if (emailField.validity.typeMismatch) {
+    showError(
+      emailErrorField,
+      "This is not a valid email address.",
+      emailField
+    );
+  }
+  if (emailField.validity.valid) {
+    removeError(emailErrorField, emailField);
+  }
 });
 
 function getCountryFieldValue() {
-    return countryField.value;
+  return countryField.value;
 }
 
-zipField.addEventListener("input", function(event) {
-    let currentCountry = getCountryFieldValue();
-    // this allows letters to be used when Great Britain is
-    // selected as the country
-    if (currentCountry === "Great Britain") {
-        zipField.setAttribute("pattern", "[A-Za-z0-9]+");
+zipField.addEventListener("input", function (event) {
+  let currentCountry = getCountryFieldValue();
+  // this allows letters to be used when Great Britain is
+  // selected as the country
+  if (currentCountry === "Great Britain") {
+    zipField.setAttribute("pattern", "[A-Za-z0-9]+");
+  }
+  if (currentCountry === "United States" || currentCountry === "Romania") {
+    zipField.setAttribute("pattern", "[0-9]*");
+  }
+  if (zipField.validity.patternMismatch) {
+    return showError(zipErrorField, "This is not a valid ZIP code.", zipField);
+  }
+  if (currentCountry === "Romania") {
+    zipField.setAttribute("minlength", "6");
+    zipField.setAttribute("maxlength", "6");
+    if (!zipField.validity.tooShort && !zipField.validity.tooLong) {
+      removeError(zipErrorField, zipField);
+    } else if (zipField.validity.tooShort) {
+      return showError(zipErrorField, "The ZIP code is too short.", zipField);
+    } else if (zipField.validity.tooLong) {
+      return showError(zipErrorField, "The ZIP code is too long.", zipField);
     }
-    if (currentCountry === "United States" || currentCountry === "Romania") {
-        zipField.setAttribute("pattern", "[0-9]*");
+  }
+  if (currentCountry === "United States") {
+    zipField.setAttribute("minlength", "5");
+    zipField.setAttribute("maxlength", "5");
+    if (!zipField.validity.tooShort && !zipField.validity.tooLong) {
+      removeError(zipErrorField, zipField);
+    } else if (zipField.validity.tooShort) {
+      showError(zipErrorField, "The ZIP code is too short.", zipField);
+      return;
+    } else if (zipField.validity.tooLong) {
+      showError(zipErrorField, "The ZIP code is too long.", zipField);
+      return;
     }
-    if (zipField.validity.patternMismatch) {
-        return showError(zipErrorField, "This is not a valid ZIP code.");
+  }
+  if (currentCountry === "Great Britain") {
+    zipField.setAttribute("minlength", "6");
+    zipField.setAttribute("maxlength", "8");
+    if (!zipField.validity.tooShort) {
+      removeError(zipErrorField, zipField);
+    } else if (zipField.validity.tooShort) {
+      return showError(zipErrorField, "The ZIP code is too short.", zipField);
     }
-    if (currentCountry === "Romania") {
-        zipField.setAttribute("minlength", "6");
-        zipField.setAttribute("maxlength", "6");
-        if (!zipField.validity.tooShort && !zipField.validity.tooLong) {
-            removeError(zipErrorField);
-        } else if (zipField.validity.tooShort) {
-            return showError(zipErrorField, "The ZIP code is too short.");
-        } else if (zipField.validity.tooLong) {
-            return showError(zipErrorField, "The ZIP code is too long.");
-        }
-    }
-    if (currentCountry === "United States") {
-        zipField.setAttribute("minlength", "5");
-        zipField.setAttribute("maxlength", "5");
-        if (!zipField.validity.tooShort && !zipField.validity.tooLong) {
-            removeError(zipErrorField);
-        } else if (zipField.validity.tooShort) {
-            showError(zipErrorField, "The ZIP code is too short.");
-            return;
-        } else if (zipField.validity.tooLong) {
-            showError(zipErrorField, "The ZIP code is too long.");
-            return;
-        }
-    }
-    if (currentCountry === "Great Britain") {
-        zipField.setAttribute("minlength", "6");
-        zipField.setAttribute("maxlength", "8");
-        if (!zipField.validity.tooShort) {
-            removeError(zipErrorField);
-        } else if (zipField.validity.tooShort) {
-            return showError(zipErrorField, "The ZIP code is too short.");
-        }
-    }
-    if (zipField.validity.valid) {
-        removeError(zipErrorField);
-    }
+  }
+  if (zipField.validity.valid) {
+    removeError(zipErrorField, zipField);
+  }
 });
 
-countryField.addEventListener("input", function(event) {
-    // this prevents incorrect ZIPs being submitted if the user
-    // changes the country
-    if (!zipField.value) return;
-    if (
-        zipField.getAttribute("pattern") === "[A-Za-z0-9]+" &&
-        countryField.value !== "Great Britain"
-    ) {
-        showError(zipErrorField, "This is not a valid ZIP code.");
-        zipField.setAttribute("pattern", "[0-9]*");
-    }
-    if (countryField.value === "United States" && zipField.value.length > 5) {
-        showError(zipErrorField, "The ZIP code is too long.");
-    } else if (
-        countryField.value === "United States" &&
-        zipField.value.length < 5
-    ) {
-        showError(zipErrorField, "The ZIP code is too short.");
-    } else {
-        removeError(zipErrorField);
-    }
-    if (countryField.value === "Romania" && zipField.value.length < 6) {
-        showError(zipErrorField, "The ZIP code is too short.");
-        zipField.setAttribute("minlength", "6");
-        zipField.setAttribute("maxlength", "6");
-    } else if (countryField.value === "Romania" && zipField.value.length > 6) {
-        showError(zipErrorField, "The ZIP code is too long.");
-    }
-    if (countryField.value === "Great Britain") {
-        zipField.setAttribute("minlength", "6");
-        zipField.setAttribute("maxlength", "8");
-    }
-    if (countryField.value === "Great Britain" && zipField.value.length < 6) {
-        showError(zipErrorField, "The ZIP code is too short.");
-    }
+countryField.addEventListener("input", function (event) {
+  // this prevents incorrect ZIPs being submitted if the user
+  // changes the country
+  if (!zipField.value) return;
+  if (
+    zipField.getAttribute("pattern") === "[A-Za-z0-9]+" &&
+    countryField.value !== "Great Britain"
+  ) {
+    showError(zipErrorField, "This is not a valid ZIP code.", zipField);
+    zipField.setAttribute("pattern", "[0-9]*");
+  }
+  if (countryField.value === "United States" && zipField.value.length > 5) {
+    showError(zipErrorField, "The ZIP code is too long.", zipField);
+  } else if (
+    countryField.value === "United States" &&
+    zipField.value.length < 5
+  ) {
+    showError(zipErrorField, "The ZIP code is too short.", zipField);
+  } else {
+    removeError(zipErrorField, zipField);
+  }
+  if (countryField.value === "Romania" && zipField.value.length < 6) {
+    showError(zipErrorField, "The ZIP code is too short.", zipField);
+    zipField.setAttribute("minlength", "6");
+    zipField.setAttribute("maxlength", "6");
+  } else if (countryField.value === "Romania" && zipField.value.length > 6) {
+    showError(zipErrorField, "The ZIP code is too long.", zipField);
+  }
+  if (countryField.value === "Great Britain") {
+    zipField.setAttribute("minlength", "6");
+    zipField.setAttribute("maxlength", "8");
+  }
+  if (countryField.value === "Great Britain" && zipField.value.length < 6) {
+    showError(zipErrorField, "The ZIP code is too short.", zipField);
+  }
 });
 
-passwordField.addEventListener("input", function(event) {
-    if (passwordField.validity.tooShort) {
-        return showError(passwordErrorField, "The password is too short.");
-    }
-    if (passwordField.validity.patternMismatch) {
-        return showError(
-            passwordErrorField,
-            "The password must contain at least one uppercase and lowercase letter, one number and one special character."
-        );
-    } else if (!passwordField.validity.patternMismatch) {
-        removeError(passwordErrorField);
-    }
+passwordField.addEventListener("input", function (event) {
+  if (passwordField.validity.tooShort) {
+    return showError(
+      passwordErrorField,
+      "The password is too short.",
+      passwordField
+    );
+  }
+  if (passwordField.validity.patternMismatch) {
+    return showError(
+      passwordErrorField,
+      "The password must contain at least one uppercase and lowercase letter, one number and one special character.",
+      passwordField
+    );
+  } else if (!passwordField.validity.patternMismatch) {
+    removeError(passwordErrorField, passwordField);
+  }
 });
 
-passwordConfirmationField.addEventListener("input", function(event) {
-    if (passwordConfirmationField.value !== passwordField.value) {
-        showError(passwordConfirmationErrorField, "The passwords to do not match.");
-    } else if (passwordConfirmationField.value === passwordField.value) {
-        removeError(passwordConfirmationErrorField);
-    }
+passwordConfirmationField.addEventListener("input", function (event) {
+  if (passwordConfirmationField.value !== passwordField.value) {
+    showError(
+      passwordConfirmationErrorField,
+      "The passwords to do not match.",
+      passwordConfirmationField
+    );
+  } else if (passwordConfirmationField.value === passwordField.value) {
+    removeError(passwordConfirmationErrorField, passwordConfirmationField);
+  }
 });
 
-form.addEventListener("submit", function(event) {
-    if (!emailField.validity.valid ||
-        !zipField.validity.valid ||
-        !countryField.validity.valid ||
-        !passwordField.validity.valid ||
-        !passwordConfirmationField.validity.valid ||
-        passwordConfirmationField.value !== passwordField.value
-    ) {
-        event.preventDefault();
-    }
+form.addEventListener("submit", function (event) {
+  if (
+    !form.checkValidity() ||
+    !emailField.checkValidity() ||
+    !zipField.checkValidity() ||
+    !passwordField.checkValidity() ||
+    !passwordConfirmationField.checkValidity() ||
+    passwordConfirmationField.value !== passwordField.value
+  ) {
+    event.preventDefault();
+  }
 });
